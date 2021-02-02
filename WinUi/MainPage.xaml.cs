9d22c0cb68bcfe98ca4d61b97cab62435f2a6cd4
Dhanasekar Mohanraj <dhanasekar.mohanraj@syncfusion.com>
2021-02-02 15:59:18 +0530

    How to hide the detailsview expander icon based on child records count in winui datagrid? sample added

---
+++ WinUi/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Syncfusion.UI.Xaml.Data;
+using Syncfusion.UI.Xaml.DataGrid;
+using System.Reflection;
+using System.Globalization;
+using Windows.Storage;
+using System.Collections.ObjectModel;
+using Microsoft.UI;
+using WinUIDemoApp.Model;
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUIDemoApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            dataGrid.DetailsViewExpanding += DataGrid_DetailsViewExpanding;
+            dataGrid.QueryDetailsViewExpanderState += DataGrid_QueryDetailsViewExpanderState;
+        }
+
+        private void DataGrid_DetailsViewExpanding(object sender, GridDetailsViewExpandingEventArgs e)
+        {
+            var employeeInfo = e.Record as Employee;
+            if (employeeInfo != null)
+            {
+                if (employeeInfo.Sales.Count == 0)
+                    e.Cancel = true;
+            }
+        }
+
+        private void DataGrid_QueryDetailsViewExpanderState(object sender, QueryDetailsViewExpanderStateEventArgs e)
+        {
+            var employeeInfo = e.Record as Employee;
+            if (employeeInfo != null)
+            {
+                if (employeeInfo.Sales.Count == 0)
+                {
+                    e.ExpanderVisibility = false;
+                }
+            }
+        }
+    }
+}
---
+++ WinUi/Model/OrderInfo.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace DetailsView_NestedLevel
+{
+    public class OrderInfo : INotifyPropertyChanged
+    {
+        #region private members
+
+        private int orderId;
+        private int _customerId;
+        private double _unitPrice;
+        private int _quantiy;
+        private int decimalDigits;
+        public int DecimalDigits
+        {
+            get { return decimalDigits; }
+            set { decimalDigits = value; RaisePropertyChanged("DecimalDigits"); }
+        }
+
+        #endregion
+
+        #region public properties
+
+        public int OrderID
+        {
+            get
+            {
+                return orderId;
+            }
+            set
+            {
+                orderId = value;
+               // RaisePropertyChanged("OrderID");
+            }
+        }
+
+        public int CustomerID
+        {
+            get
+            {
+                return _customerId;
+            }
+            set
+            {
+                _customerId = value;
+                RaisePropertyChanged("CustomerID");
+            }
+        }
+
+        public double UnitPrice
+        {
+            get
+            {
+                return _unitPrice;
+            }
+            set
+            {
+                _unitPrice = value;
+                RaisePropertyChanged("UnitPrice");
+            }
+        }
+        [Range(105, 117, ErrorMessage = "Quantity can hold value between 105 and 117")]
+
+        public int Quantity
+        {
+            get
+            {
+                return _quantiy;
+            }
+            set
+            {
+                _quantiy = value;
+                RaisePropertyChanged("Quantity");
+            }
+        }
+
+        private ObservableCollection<ProductInfo> productdetails;
+
+        public ObservableCollection<ProductInfo> ProductDetails
+        {
+            get
+            {
+                return productdetails;
+            }
+            set
+            {
+                productdetails = value;
+                RaisePropertyChanged("ProductDetails");
+            }
+        }
+
+        #endregion
+
+        public void RaisePropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //public string Error
+        //{
+        //    get { return null; }
+        //}
+
+        //public string this[string columnName]
+        //{
+        //    get
+        //    {
+        //        if (columnName.Equals("UnitPrice"))
+        //        {
+        //            if (this.UnitPrice > 109)
+        //                return null;
+        //            else
+        //                return "UnitPrice sholud less than 109";
+        //        }
+        //        return null;
+        //    }
+        //}
+    }
+}
